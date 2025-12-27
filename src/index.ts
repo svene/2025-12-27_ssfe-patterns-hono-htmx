71e3ada336f7596d24aecba35858380e2e66f59e
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2025-12-27 18:13:34 +0100

    first html endpoint using html helper

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import { Hono } from 'hono'
+import { html } from "hono/html"
 
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.html('Hello Hono!')
+})
+
+app.get('/hello/:username', (c) => {
+  const { username } = c.req.param()
+  return c.html(
+      html`<!doctype html>
+      <h1>Hello! ${username}!</h1>`
+  )
 })
 
 export default app
