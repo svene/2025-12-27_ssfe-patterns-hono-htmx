4937421e2978193fc3c8ca22fa84d4833f25da14
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2025-12-27 19:56:23 +0100

    real JSX variant of hello-world

--- src/hello-jsx.tsx
+++ src/hello-jsx.tsx
@@ -1,13 +1,20 @@
 import {Hono} from "hono";
-import { html } from "hono/html"
 
 export function hello_jsx_init(app: Hono) {
 	app.get('/hellojsx/:username', (c) => {
-		const { username } = c.req.param()
-		return c.html(
-			html`<!doctype html>
-      <h1>Hello from JSX! ${username}!</h1>`
-		)
+		const { username } = c.req.param();
+		const View = () => {
+			return (
+				<html>
+				<body>
+				<h1>Hello from JSX! {username}!</h1>
+				</body>
+				</html>
+			)
+		}
+
+		return c.html(<View/>)
+
 	})
 
 }
