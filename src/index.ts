2177f0a62dac6086ea76d4b4a7d1266b51d5b776
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2025-12-28 08:35:52 +0100

    main page: continued with structure

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,21 @@
 import { Hono } from 'hono'
-import {hello_html_init} from "./m00hello/hello-html";
 import {html} from "hono/html";
-import {hello_jsx_init} from "./m00hello/hello-jsx";
-import {M01D01, m01d01_init} from "./m01html/m01d01";
+import {m01_init, m01_menu} from "./m01html/m01";
+import {m00_init, m00_menu} from "./m00hello/m00";
 
 const app = new Hono()
 
 app.get('/', (c) => {
   return c.html(
       html`<!doctype html>
-      <h1>First Steps</h1>
-      <ul>
-        <li><a href="/hello/html" target="_blank">Hello HTML</a> </li>
-        <li><a href="/hellojsx/jsx" target="_blank">Hello JSX</a> </li>
-      </ul>
-      <h1>HTML Helper</h1>
-      <ul>
-        <li><a href="${M01D01.URL}" target="_blank">Hello HTML</a> </li>
-      </ul>
+      ${m00_menu()}
+      ${m01_menu()}
       <h1>JSX</h1>
-      <ul>
-          <li><a href="${M01D01.URL}" target="_blank">Hello JSX</a> </li>
-      </ul>
       `
   )
 })
-hello_html_init(app);
-hello_jsx_init(app);
-m01d01_init(app);
+m00_init(app);
+m01_init(app);
 
 export default app
 
---
+++ src/m00hello/m00.ts
@@ -0,0 +1,20 @@
+import {Hono} from "hono";
+import {html} from "hono/html";
+import {hello_html_init} from "./hello-html";
+import {hello_jsx_init} from "./hello-jsx";
+
+
+export function m00_init(app: Hono) {
+	hello_html_init(app);
+	hello_jsx_init(app);
+}
+
+export function m00_menu() {
+	return html`
+		<h1>First Steps</h1>
+		<ul>
+			<li><a href="/hello/html" target="_blank">Hello HTML</a> </li>
+			<li><a href="/hellojsx/jsx" target="_blank">Hello JSX</a> </li>
+		</ul>
+      `
+}
