a9ff2ffe930a281b99dca2d4a5974ed68b8a0659
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2025-12-28 08:59:01 +0100

    M01D01: working

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,11 @@ import { Hono } from 'hono'
 import {html} from "hono/html";
 import {m01_init, m01_menu} from "./m01html/m01";
 import {m00_init, m00_menu} from "./m00hello/m00";
+import { serveStatic } from 'hono/bun'
 
 const app = new Hono()
+
+app.use('/static/*', serveStatic({ root: './' }))
 
 app.get('/', (c) => {
   return c.html(
--- src/m01html/m01d01.ts
+++ src/m01html/m01d01.ts
@@ -7,8 +7,21 @@ export const M01D01 = {
 export function m01d01_init(app: Hono) {
 	app.get(M01D01.URL, (c) => {
 		return c.html(
-			html`<!doctype html>
-      <h1>Hello from M01D01!</h1>`
+			html`<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<link rel="stylesheet" href="/static/css/simplepage.css"/>
+			</head>
+
+			<body class="page">
+			<div>
+				<h1>Application Page</h1>
+
+<!--				{#include dev/svenehrke/demo/ssfepatterns/m01plain/m01d01_code}{/include}-->
+			</div>
+			</body>
+			</html>`
 		)
 	})
 
