4fb3dc0da71046979337888237cb52c41f1f6183
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2025-12-28 18:26:16 +0100

    added support for bulma and htmx in layoutpage bulmapage.tsx

--- src/c00jsxcomponents/bulmapage.tsx
+++ src/c00jsxcomponents/bulmapage.tsx
@@ -1,15 +1,17 @@
 import type { FC } from 'hono/jsx'
+import {HEAD} from "../components/head";
 
 export const BulmaPage: FC<{ title?: string; children?: any }> = ({ title, children }) => (
 	<html lang="en">
-	<head>
-		<meta charset="utf-8" />
-		<title>{title ?? 'My App'}</title>
-		<link rel="stylesheet" href="/static/css/simplepage.css" />
-	</head>
+	{HEAD}
 
-	<body class="page">
-		{children}
+	<body>
+	<div className="container mt-1">
+
+		<div className="p-1 mt-1">
+			{children}
+		</div>
+	</div>
 	</body>
 	</html>
 )
